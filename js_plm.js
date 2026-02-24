ccf83956449092e4da18b57ef79de914b78a3a69
Mohammad Saif Al Islam <saif404.ist@gmail.com>
2026-02-24 14:50:10 +0600

    Update js_plm.js

--- js_plm.js
+++ js_plm.js
@@ -38,5 +38,14 @@ if (isPrime) {
 ‎for (let i = 1; i <= 10; i++) {
 ‎    console.log(i);
 ‎}
+
+‎//  Write a JavaScript program to find the sum of first 10 natural numbers.
+‎
+‎let sum = 0;
+‎for (let i = 1; i <= 10; i++) {
+‎    sum += i;
+‎}
+‎console.log("Sum is: " + sum);
+‎
 ‎
 ‎
