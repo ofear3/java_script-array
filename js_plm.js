4bd6e239663925d30dcc3d73f69e0f8b477751f8
Mohammad Saif Al Islam <saif404.ist@gmail.com>
2026-03-01 06:05:45 +0600

    Add multiplication table program and update comments
    Added a program to print the multiplication table of 5 and updated comments for clarity.

--- js_plm.js
+++ js_plm.js
@@ -80,7 +80,7 @@ if (isPrime) {
 ‎}
 
 
-‎// 6. Write a JavaScript program to check if a number is positive, negative or zero.
+‎//  Write a JavaScript program to check if a number is positive, negative or zero.
 ‎
 ‎let n = -5;
 ‎
@@ -91,3 +91,12 @@ if (isPrime) {
 ‎} else {
 ‎    console.log("Zero");
 ‎}
+
+‎//  Write a JavaScript program to print multiplication table of 5.
+‎
+‎let table = 5;
+‎
+‎for (let i = 1; i <= 10; i++) {
+‎    console.log(table + " x " + i + " = " + (table * i));
+‎}
+‎
