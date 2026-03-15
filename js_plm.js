b55b9baa28d370ac3828b3d6d1da1eeef7b22752
Mohammad Saif Al Islam <saif404.ist@gmail.com>
2026-03-15 12:50:16 +0600

    Update js_plm.js

--- js_plm.js
+++ js_plm.js
@@ -256,3 +256,9 @@ let arr = [1,2,3,4,5]
 ‎}
 ‎
 ‎console.log("Digit count: " + digitCount);
+
+‎//  Write a JavaScript program to find the square of numbers from 1 to 10.
+‎
+‎for (let i = 1; i <= 10; i++) {
+‎    console.log(i + " square = " + (i * i));
+‎}
