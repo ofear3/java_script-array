2d276b378a2312bcd384c2e9cebad6276fa9bc3a
Mohammad Saif Al Islam <saif404.ist@gmail.com>
2026-03-16 12:46:41 +0600

    Update js_plm.js

--- js_plm.js
+++ js_plm.js
@@ -262,3 +262,9 @@ let arr = [1,2,3,4,5]
 ‎for (let i = 1; i <= 10; i++) {
 ‎    console.log(i + " square = " + (i * i));
 ‎}
+
+‎//  Write a JavaScript program to find the cube of numbers from 1 to 10.
+‎
+‎for (let i = 1; i <= 10; i++) {
+‎    console.log(i + " cube = " + (i * i * i));
+‎}
