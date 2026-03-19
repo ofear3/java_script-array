45e77b392ec290bcc6fae5e443ea7eb4144f8f1f
Mohammad Saif Al Islam <saif404.ist@gmail.com>
2026-03-19 15:08:28 +0600

    Add loop to print even numbers from 1 to 50

--- js_plm.js
+++ js_plm.js
@@ -286,4 +286,12 @@ let arr = [1,2,3,4,5]
 ‎}
 ‎
 ‎console.log("Sum of even numbers: " + sumEven);
+
+‎// 25. Write a JavaScript program to print all even numbers from 1 to 50.
+‎
+‎for (let i = 1; i <= 50; i++) {
+‎    if (i % 2 === 0) {
+‎        console.log(i);
+‎    }
+‎}
 ‎
