082ace36f510f2fab76cfe358d3a26bd0799f9fe
Mohammad Saif Al Islam <saif404.ist@gmail.com>
2026-03-20 15:53:49 +0600

    Add program to print all odd numbers from 1 to 50

--- js_plm.js
+++ js_plm.js
@@ -295,3 +295,11 @@ let arr = [1,2,3,4,5]
 ‎    }
 ‎}
 ‎
+‎//  Write a JavaScript program to print all odd numbers from 1 to 50.
+‎
+‎for (let i = 1; i <= 50; i++) {
+‎    if (i % 2 !== 0) {
+‎        console.log(i);
+‎    }
+‎}
+‎
