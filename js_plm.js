06871dc54290883659efc1c97af9d8b32cc18285
Mohammad Saif Al Islam <saif404.ist@gmail.com>
2026-04-01 14:22:43 +0600

    Implement minimum number finder in js_plm.js
    Added a program to find the minimum number in an array.

--- js_plm.js
+++ js_plm.js
@@ -416,3 +416,17 @@ let arr = [1,2,3,4,5]
 ‎}
 ‎
 ‎console.log("Maximum = " + max);
+
+‎//  Write a JavaScript program to find the minimum number in an array.
+‎
+‎let min = arr[0];
+‎
+‎for (let i = 1; i < arr.length; i++) {
+‎    if (arr[i] < min) {
+‎        min = arr[i];
+‎    }
+‎}
+‎
+‎console.log("Minimum = " + min);
+‎
+‎
