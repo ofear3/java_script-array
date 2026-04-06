dc46010c12fba6afb6c4103b2779424eddf25ff8
Mohammad Saif Al Islam <saif404.ist@gmail.com>
2026-04-06 10:44:27 +0600

    Add sorting functionality for an array

--- js_plm.js
+++ js_plm.js
@@ -485,3 +485,21 @@ let arr = [1,2,3,4,5]
 ‎
 ‎console.log("Unique array:", unique);
 ‎
+
+‎//  Write a JavaScript program to sort an array (ascending).
+‎
+‎let arr2 = [5, 2, 9, 1, 7];
+‎
+‎for (let i = 0; i < arr2.length; i++) {
+‎    for (let j = i + 1; j < arr2.length; j++) {
+‎        if (arr2[i] > arr2[j]) {
+‎            let temp = arr2[i];
+‎            arr2[i] = arr2[j];
+‎            arr2[j] = temp;
+‎        }
+‎    }
+‎}
+‎
+‎console.log("Sorted:", arr2);
+‎
+‎
