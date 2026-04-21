7af3d1f1673dc83a20635a8cd515473f614845b5
Mohammad Saif Al Islam <saif404.ist@gmail.com>
2026-04-21 22:43:24 +0600

    Implement Armstrong number check in js_plm.js
    Added a program to check if a number is an Armstrong number.

--- js_plm.js
+++ js_plm.js
@@ -657,3 +657,21 @@ let arr = [1,2,3,4,5]
 ‎let arrObj = Object.entries(obj3);
 ‎
 ‎console.log(arrObj);‎
+
+‎//  Write a JavaScript program to check if a number is Armstrong number.
+‎
+‎let num = 153;
+‎let temp = num;
+‎let sum = 0;
+‎
+‎while (temp > 0) {
+‎    let digit = temp % 10;
+‎    sum += digit * digit * digit;
+‎    temp = Math.floor(temp / 10);
+‎}
+‎
+‎if (sum === num) {
+‎    console.log("Armstrong");
+‎} else {
+‎    console.log("Not Armstrong");
+‎}
