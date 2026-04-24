f7daa72ab5ecc880192ca556ee6fd02ec0c2238f
Mohammad Saif Al Islam <saif404.ist@gmail.com>
2026-04-24 19:45:11 +0600

    Implement perfect number check in js_plm.js
    Added a program to check for perfect numbers.

--- js_plm.js
+++ js_plm.js
@@ -701,3 +701,20 @@ let arr = [1,2,3,4,5]
 ‎    max++;
 ‎}
 ‎
+‎//  Write a JavaScript program to check if a number is perfect number.
+‎
+‎let number = 28;
+‎let sum = 0;
+‎
+‎for (let i = 1; i < number; i++) {
+‎    if (number % i === 0) {
+‎        sum += i;
+‎    }
+‎}
+‎
+‎if (sum === number) {
+‎    console.log("Perfect number");
+‎} else {
+‎    console.log("Not perfect");
+‎}
+‎
