d0b5e52258c80da465bf333da774800c6ed6f1e9
Mohammad Saif Al Islam <saif404.ist@gmail.com>
2026-04-30 21:07:28 +0600

    Implement even and odd number counter in array
    Added a program to count even and odd numbers in an array.

--- js_plm.js
+++ js_plm.js
@@ -786,3 +786,15 @@ let arr = [1,2,3,4,5]
 ‎        console.log(i);
 ‎    }
 ‎}
+
+‎//  Write a JavaScript program to count even and odd numbers in array.
+‎
+‎let arr = [1,2,3,4,5,6];
+‎let even = 0, odd = 0;
+‎
+‎for (let i = 0; i < arr.length; i++) {
+‎    if (arr[i] % 2 === 0) even++;
+‎    else odd++;
+‎}
+‎
+‎console.log("Even:", even, "Odd:", odd);
