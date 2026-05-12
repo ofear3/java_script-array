71883752a9f809373d3139d7279590fe14909bee
Mohammad Saif Al Islam <saif404.ist@gmail.com>
2026-05-12 20:20:09 +0600

    Implement factorial calculation in js_plm.js
    Added a JavaScript program to calculate the factorial of a number.

--- js_plm.js
+++ js_plm.js
@@ -896,3 +896,14 @@ let arr = [1,2,3,4,5]
 ‎
 ‎console.log(main.includes(sub));
 ‎
+// Write a JavaScript program to find the factorial of a number.
+
+let number = 5;
+let factorial = 1;
+
+for (let i = 1; i <= number; i++) {
+    factorial = factorial * i;
+}
+
+console.log("Factorial of " + number + " is: i" + factorial);
+
