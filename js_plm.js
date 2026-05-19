908a7c6d7769a53a3a21606aa8c8acd2fc41bf8b
Mohammad Saif Al Islam <saif404.ist@gmail.com>
2026-05-19 19:05:58 +0600

    Update js_plm.js

--- js_plm.js
+++ js_plm.js
@@ -971,3 +971,13 @@ console.log("Factorial of " + number + " is: i" + factorial);
 ‎}
 ‎
 ‎console.log("Reversed: " + reversed);
+
+
+//  Write a JavaScript program to find the factorial of a number.
+‎
+‎let num = 5;
+‎let fact = 1;
+‎for (let i = 1; i <= num; i++) {
+‎    fact *= i;
+‎}
+‎console.log("Factorial is: " + fact);
