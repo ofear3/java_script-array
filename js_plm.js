eee2717350a2cb03f601865399e6cf6cf5283f3a
Mohammad Saif Al Islam <saif404.ist@gmail.com>
2026-05-21 22:03:34 +0600

    Add program to calculate sum of first 10 natural numbers

--- js_plm.js
+++ js_plm.js
@@ -994,3 +994,13 @@ for (let i = 1; i <= number; i++) {
 
 console.log("Factorial of " + number + " is: i" + factorial);
 
+
+‎//  Write a JavaScript program to find the sum of first 10 natural numbers.
+‎
+‎let sum = 0;
+‎for (let i = 1; i <= 10; i++) {
+‎    sum += i;
+‎}
+‎console.log("Sum is: " + sum);
+‎
+
