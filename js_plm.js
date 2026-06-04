d84ace5eae679ab6e3e31020af0f045c5c1ddf80
Mohammad Saif Al Islam <saif404.ist@gmail.com>
2026-06-04 09:00:40 +0600

    Add program to check if a number is even or odd

--- js_plm.js
+++ js_plm.js
@@ -1160,3 +1160,13 @@ let arr = [1,2,3,4,5]
 ‎}
 ‎
 ‎console.log("Array sum: " + total);
+
+‎//  Write a JavaScript program to check if a number is even or odd.
+‎
+‎let number = 7;
+‎if (number % 2 === 0) {
+‎    console.log("Even");
+‎} else {
+‎    console.log("Odd");
+‎}
+
