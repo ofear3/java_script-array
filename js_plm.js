2f0a7251c3185a1f056c824ffd7422bc3fe252f9
Mohammad Saif Al Islam <saif404.ist@gmail.com>
2026-06-06 19:45:54 +0600

    Implement square calculation for numbers 1 to 10
    Added a program to calculate and display the square of numbers from 1 to 10.

--- js_plm.js
+++ js_plm.js
@@ -1175,3 +1175,10 @@ let arr = [1,2,3,4,5]
 ‎for (let i = 1; i <= 10; i++) {
 ‎    console.log(i);
 ‎}
+
+
+‎//  Write a JavaScript program to find the square of numbers from 1 to 10.
+‎
+‎for (let i = 1; i <= 10; i++) {
+‎    console.log(i + " square = " + (i * i));
+‎}
