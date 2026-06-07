029175e51554ba308cfa510ad972517ad59aed5d
Mohammad Saif Al Islam <saif404.ist@gmail.com>
2026-06-07 21:24:09 +0600

    Add program to check if a number is positive, negative, or zero

--- js_plm.js
+++ js_plm.js
@@ -1182,3 +1182,16 @@ let arr = [1,2,3,4,5]
 ‎for (let i = 1; i <= 10; i++) {
 ‎    console.log(i + " square = " + (i * i));
 ‎}
+
+//  Write a JavaScript program to check if a number is positive, negative or zero.
+‎
+‎let n = -5;
+‎
+‎if (n > 0) {
+‎    console.log("Positive");
+‎} else if (n < 0) {
+‎    console.log("Negative");
+‎} else {
+‎    console.log("Zero");
+‎}
+
