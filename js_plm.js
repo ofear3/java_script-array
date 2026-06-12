556c27544a21b50bdeabb67af609a71fa7eb6b1d
Mohammad Saif Al Islam <saif404.ist@gmail.com>
2026-06-12 19:09:06 +0600

    Update js_plm.js

--- js_plm.js
+++ js_plm.js
@@ -1231,3 +1231,14 @@ let arr = [1,2,3,4,5]
 ‎} else {
 ‎    console.log("Odd");
 ‎}
+
+
+‎//  Write a JavaScript program to find the sum of first 10 natural numbers.
+‎
+‎let sum = 0;
+‎for (let i = 1; i <= 10; i++) {
+‎    sum += i;
+‎}
+‎console.log("Sum is: " + sum);
+‎
+‎
