993e1e946c1c9d1157e026d9da317917cdda0bc5
Mohammad Saif Al Islam <saif404.ist@gmail.com>
2026-06-17 09:55:22 +0600

    Update js_plm.js

--- js_plm.js
+++ js_plm.js
@@ -1275,3 +1275,14 @@ let arr = [1,2,3,4,5]
 ‎
 ‎console.log("x: " + x + ", y: " + y);
 
+‎//  Write a JavaScript program to check if a number is positive, negative or zero.
+‎
+‎let n = -5;
+‎
+‎if (n > 0) {
+‎    console.log("Positive");
+‎} else if (n < 0) {
+‎    console.log("Negative");
+‎} else {
+‎    console.log("Zero");
+‎}
