d8a2a6beaf678601d9460e7aceb1024ce1c39f9a
Mohammad Saif Al Islam <saif404.ist@gmail.com>
2026-06-24 09:49:20 +0600

    Add array sum calculation to js_plm.js

--- js_plm.js
+++ js_plm.js
@@ -1356,3 +1356,14 @@ let arr = [1,2,3,4,5]
 ‎
 ‎console.log("Average = " + avg);
 
+‎//  Write a JavaScript program to find the sum of elements in an array.
+‎
+let arr = [1,2,3,4,5]
+
+‎let total = 0;
+‎
+‎for (let i = 0; i < arr.length; i++) {
+‎    total += arr[i];
+‎}
+‎
+‎console.log("Array sum: " + total);
