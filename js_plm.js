cc58e9a67dc2e2b3fafe224130761ef4c0ee5360
Mohammad Saif Al Islam <saif404.ist@gmail.com>
2026-06-30 17:30:09 +0600

    Implement even or odd check in js_plm.js
    Added a program to check if a number is even or odd.

--- js_plm.js
+++ js_plm.js
@@ -1418,3 +1418,12 @@ let arr = [1,2,3,4,5]
 ‎    console.log("Largest is: " + b);
 ‎}
 
+
+//  Write a JavaScript program to check if a number is even or odd.
+‎
+‎let number = 7;
+‎if (number % 2 === 0) {
+‎    console.log("Even");
+‎} else {
+‎    console.log("Odd");
+‎}
