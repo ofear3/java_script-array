14219ea51c70bbd9735acb15446a52621d7a0027
Mohammad Saif Al Islam <saif404.ist@gmail.com>
2026-07-07 07:15:45 +0600

    Update js_plm.js

--- js_plm.js
+++ js_plm.js
@@ -1492,3 +1492,10 @@ console.log("Factorial of " + number + " is: i" + factorial);
 ‎} else {
 ‎    console.log("Odd");
 ‎}‎
+
+
+// Write a JavaScript program to print numbers from 1 to 10.
+‎
+‎for (let i = 1; i <= 10; i++) {
+‎    console.log(i);
+‎}
