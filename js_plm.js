5eabba829a4fc7b3bb59d0953ee4b7aa3271ee8b
Mohammad Saif Al Islam <saif404.ist@gmail.com>
2026-07-10 20:52:53 +0600

    Add JavaScript programs for sum and factorial

--- js_plm.js
+++ js_plm.js
@@ -47,6 +47,14 @@ if (isPrime) {
 ‎}
 ‎console.log("Sum is: " + sum);
 ‎
+‎‎//  Write a JavaScript program to find the sum of first 10 natural numbers.
+‎
+‎let sum = 0;
+‎for (let i = 1; i <= 10; i++) {
+‎    sum += i;
+‎}
+‎console.log("Sum is: " + sum);
+‎
 ‎
 ‎//  Write a JavaScript program to find the factorial of a number.
 ‎
