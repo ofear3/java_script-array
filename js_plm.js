23571eb166cab686fb32a4b51eb122dcfd5ff73f
Mohammad Saif Al Islam <saif404.ist@gmail.com>
2026-07-12 19:09:10 +0600

    Implement loop to print numbers from 1 to 10
    Added a loop to print numbers from 1 to 10.

--- js_plm.js
+++ js_plm.js
@@ -1534,3 +1534,9 @@ console.log("Factorial of " + number + " is: i" + factorial);
 ‎    sum += i;
 ‎}
 ‎console.log("Sum is: " + sum);
+
+‎// Write a JavaScript program to print numbers from 1 to 10.
+‎
+‎for (let i = 1; i <= 10; i++) {
+‎    console.log(i);
+‎}
