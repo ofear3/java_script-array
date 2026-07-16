ed27c9bbb084fc6958874e254b09b329f41da90d
Mohammad Saif Al Islam <saif404.ist@gmail.com>
2026-07-16 20:16:06 +0600

    Update js_plm.js

--- js_plm.js
+++ js_plm.js
@@ -123,6 +123,14 @@ if (isPrime) {
 ‎for (let i = 1; i <= 10; i++) {
 ‎    console.log(table + " x " + i + " = " + (table * i));
 ‎}
+
+//  Write a JavaScript program to print multiplication table of 5.
+‎
+‎let table = 5;
+‎
+‎for (let i = 1; i <= 10; i++) {
+‎    console.log(table + " x " + i + " = " + (table * i));
+‎}
 ‎
 ‎//  Write a JavaScript program to reverse a string.
 ‎
