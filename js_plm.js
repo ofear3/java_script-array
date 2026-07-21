4026710bf1c414be19fcf9409facebc9418e476f
Mohammad Saif Al Islam <saif404.ist@gmail.com>
2026-07-21 22:42:02 +0600

    Add comment and duplicate loop to print numbers

--- js_plm.js
+++ js_plm.js
@@ -1581,3 +1581,9 @@ console.log("Factorial of " + number + " is: i" + factorial);
 ‎for (let i = 1; i <= 10; i++) {
 ‎    console.log(i);
 ‎}
+
+‎// Write a JavaScript program to print numbers from 1 to 10.
+‎
+‎for (let i = 1; i <= 10; i++) {
+‎    console.log(i);
+‎}
