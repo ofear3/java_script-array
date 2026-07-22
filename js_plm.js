80850654cdcaae15ea8f53e7bace6745a7dc45c0
Mohammad Saif Al Islam <saif404.ist@gmail.com>
2026-07-22 20:09:19 +0600

    Implement number sign check in js_plm.js
    Add a program to check if a number is positive, negative, or zero.

--- js_plm.js
+++ js_plm.js
@@ -1587,3 +1587,15 @@ console.log("Factorial of " + number + " is: i" + factorial);
 ‎for (let i = 1; i <= 10; i++) {
 ‎    console.log(i);
 ‎}
+
+‎//  Write a JavaScript program to check if a number is positive, negative or zero.
+‎
+‎let n = -5;
+‎
+‎if (n > 0) {
+‎    console.log("Positive");
+‎} else if (n < 0) {
+‎    console.log("Negative");
+‎} else {
+‎    console.log("Zero");
+‎}
