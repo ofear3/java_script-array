d9259d87a6c7072f2aa29951fac4188ee6dccac6
Mohammad Saif Al Islam <saif404.ist@gmail.com>
2026-08-01 22:07:29 +0600

    Add loop to print numbers from 1 to 10
    Added a loop to print numbers from 1 to 10.

--- js_plm.js
+++ js_plm.js
@@ -378,6 +378,12 @@ let arr = [1,2,3,4,5]
 ‎console.log("y = " + y);
 
 ‎
+// a JavaScript program to print numbers from 1 to 10.
+‎
+‎for (let i = 1; i <= 10; i++) {
+‎    console.log(i);
+‎}
+
 ‎//  Write a JavaScript program to find the length of a string.
 ‎
 ‎let str = "JavaScript";
